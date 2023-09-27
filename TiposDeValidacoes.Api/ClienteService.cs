f20452d02c0c295cf622e26e3fd99e974d84e99f
Jose Nery <josenerydev@gmail.com>
2023-09-27 17:07:42 -0300

    GerarMensagemErro

--- TiposDeValidacoes.Api/ClienteService.cs
+++ TiposDeValidacoes.Api/ClienteService.cs
@@ -6,18 +6,30 @@ namespace TiposDeValidacoes.Api
     {
         public Dictionary<string, ValidationResult> ProcessarCliente(Cliente cliente)
         {
-            var resultados = new ValidationBuilder<Cliente>(cliente)
+            var validationBuilder = new ValidationBuilder<Cliente>(cliente)
                 .AdicionarValidacao(c => c.Cpf, valor => !string.IsNullOrWhiteSpace(valor as string) && valor.ToString().Length == 11)
                 .AdicionarValidacao(c => c.Nome)
                 .AdicionarValidacao(c => c.Endereco.CEP)
                 .AdicionarValidacao(c => c.Tipo, valor => (TipoPessoa)valor != TipoPessoa.NaoDefinido)
-                .AdicionarValidacao(c => c.Telefones)
-                .ValidarPropriedades()
-                .Invalidos();
+                .AdicionarValidacao(c => c.Telefones);
 
-            // Lógica adicional de processamento, se necessário
+            var resultados = validationBuilder.ValidarPropriedades();
+            var resultadosInvalidos = resultados.Invalidos();
+
+            if (resultadosInvalidos.Any())
+            {
+                var mensagemErro = GerarMensagemErro(resultadosInvalidos);
+                throw new ArgumentException(mensagemErro);
+            }
 
             return resultados;
+        }
+
+        private string GerarMensagemErro(Dictionary<string, ValidationResult> resultadosInvalidos)
+        {
+            var nomesCamposInvalidos = resultadosInvalidos.Keys.ToList();
+            var listaCamposInvalidos = string.Join(", ", nomesCamposInvalidos);
+            return $"Erro ao salvar o cliente. Os campos {listaCamposInvalidos} são obrigatórios.";
         }
     }
 }
