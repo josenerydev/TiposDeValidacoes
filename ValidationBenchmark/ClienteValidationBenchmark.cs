d9df005f65b013d06ecb08d9b87d6e8bc7a5415f
Jose Nery <josenerydev@gmail.com>
2023-09-27 18:30:54 -0300

    antes

---
+++ ValidationBenchmark/ClienteValidationBenchmark.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+using TiposDeValidacoes.Api;
+
+namespace ValidationBenchmark
+{
+    [MemoryDiagnoser]
+    public class ClienteValidationBenchmark
+    {
+        private readonly ValidationBuilder<Cliente> _validationBuilder;
+
+        public ClienteValidationBenchmark()
+        {
+            var cliente = new Cliente
+            {
+                Nome = "João",
+                Cpf = "123.456.789-09",
+                Tipo = TipoPessoa.Fisica,
+                Endereco = new Endereco
+                {
+                    CEP = "12345-678",
+                    Logradouro = "Rua Exemplo"
+                },
+                Telefones = new List<Telefone>
+            {
+                new Telefone { DDD = "11", Numero = "987654321" },
+                new Telefone { DDD = "21", Numero = "123456789" }
+            }
+            };
+
+            _validationBuilder = new ValidationBuilder<Cliente>(cliente)
+                .AdicionarValidacao(c => c.Nome)
+                .AdicionarValidacao(c => c.Cpf)
+                .AdicionarValidacao(c => c.Tipo)
+                .AdicionarValidacao(c => c.Endereco)
+                .AdicionarValidacao(c => c.Telefones);
+        }
+
+        [Benchmark]
+        public void ValidarClienteBenchmark()
+        {
+            _validationBuilder.ValidarPropriedades();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ClienteValidationBenchmark>();
+        }
+    }
+}
---
+++ ValidationBenchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+using ValidationBenchmark;
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Iniciando Benchmark...");
+var summary = BenchmarkRunner.Run<ClienteValidationBenchmark>();
+Console.WriteLine("Benchmark concluído.");
